6b0aa31df0418a4842d5bc52aaae83d765b81cc4
Madhav Biju <madhavbiju01@gmail.com>
2023-02-12 23:45:30 +0530

    Initial Home Screen Bring Up

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:annahasta/main.dart';
 import 'package:annahasta/Functions/bottomnav.dart';
 import 'package:annahasta/Screens/profile.dart';
+import 'package:annahasta/models/cont_model.dart';
+import '../models/remote_data_source/firestore_helper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -38,6 +40,79 @@ class _HomePageState extends State<HomePage> {
         automaticallyImplyLeading: false,
       ),
       bottomNavigationBar: BottomNav(selectedIndex: 0),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            StreamBuilder<List<ContModel>>(
+                stream: FirestoreHelper.read(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (snapshot.hasError) {
+                    return Center(
+                      child: Text("some error occured"),
+                    );
+                  }
+                  if (snapshot.hasData) {
+                    final userData = snapshot.data;
+                    return Expanded(
+                      child: ListView.builder(
+                          itemCount: userData!.length,
+                          itemBuilder: (context, index) {
+                            final singleUser = userData[index];
+                            return Container(
+                              margin: EdgeInsets.symmetric(vertical: 5),
+                              child: ListTile(
+                                onLongPress: () {
+                                  showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        return AlertDialog(
+                                          title: Text("Delete"),
+                                          content: Text(
+                                              "Are you sure you want to delete"),
+                                          actions: [
+                                            ElevatedButton(
+                                                style: const ButtonStyle(
+                                                  backgroundColor:
+                                                      MaterialStatePropertyAll<
+                                                              Color>(
+                                                          Color.fromARGB(255,
+                                                              198, 40, 40)),
+                                                ),
+                                                onPressed: () {
+                                                  FirestoreHelper.delete(
+                                                          singleUser)
+                                                      .then((value) {
+                                                    Navigator.pop(context);
+                                                  });
+                                                },
+                                                child: Text("Delete"))
+                                          ],
+                                        );
+                                      });
+                                },
+                                title: Text("Location: ${singleUser.boxID}"),
+                                subtitle:
+                                    Text("Date & Time: ${singleUser.contents}"),
+                                trailing: InkWell(
+                                    onTap: () {}, child: Icon(Icons.edit)),
+                              ),
+                            );
+                          }),
+                    );
+                  }
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                })
+          ],
+        ),
+      ),
     );
   }
 }
